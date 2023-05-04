0fd256cb58b31a302180204307f9adc342fffcfa
Kritim <kritimstha2015@gmail.com>
2023-05-04 12:33:59 +0545

    first commit

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
         Route::resource('details',DetailController::class);
-        // Route::delete('/details/{id}', [DetailController::class, 'destroy'])->name('details.destroy');
+        
        
